eba02b5ceae6a9fdb88c3c380707f6834e0bf98e
Deepakcode607 <120246946+Deepakcode607@users.noreply.github.com>
2024-09-01 11:50:26 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import BmiCalculator from './BmiCalculator';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BmiCalculator />
+    </div>
+  );
+}
+
+export default App;
